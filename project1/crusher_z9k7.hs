3e476d1e6cf664d66fbe79c10bce610187d8ce04
Jaxsun <jaxsunm@gmail.com>
2014-11-02 16:03:18 -0800

    added heuristics and some helpers

--- project1/crusher_z9k7.hs
+++ project1/crusher_z9k7.hs
@@ -1,6 +1,11 @@
+type Row = String
+type Board = [Row]
+
 main = print $ out
 
-a = parse_current_board "WWW-WW-------BB-BBB" 3
+s = "WWW-WW-------BB-BBB"
+z = 3
+a = parse_current_board s z
 out = rotate_board (rotate_board a)
 
 
@@ -15,7 +20,12 @@ parse_current_board_helper board_string board_size current_row result
     | otherwise = parse_current_board_helper (drop row board_string) board_size (current_row + 1) ((take row board_string):result)
     where
         row = row_size board_size current_row
-
+		
+-- Board Serializer
+-- Input  : A list of strings representing the rows of a board
+-- Output : A String which represents the input board
+dump_board:: Board -> String
+dump_board board = foldl (++) "" board
 
 -- Board Rotator
 --     Takes successive slices of the board and cons them into a result list
@@ -42,6 +52,35 @@ slice_board_helper (x:xs) rest_of_board row remainder
     | null xs = ((reverse ((head x):row)),(((tail x):remainder) ++ rest_of_board))
     | otherwise = slice_board_helper xs rest_of_board ((head x):row) ((tail x): remainder)
 
+-- Board Multi Rotator
+-- Rotates a board the given number of times
+rotate_board_multi:: Board -> Int -> Board
+rotate_board_multi board 0 = board
+rotate_board_multi board x = rotate_board_multi (rotate_board board) (x-1)
+
+-- Heuristics --
+-- Basic Heuristic 
+-- it really likes to win
+-- it really doesn't like to lose
+-- it likes having pieces
+rate_board:: Board -> Char -> Int
+rate_board board player
+    | is_win_for  player board  = 999
+    | is_loss_for player board  = -999
+    | otherwise                 = count player board
+    
+is_win_for:: Char -> Board -> Bool
+is_win_for  player board = is_loss_for (other player) board
+is_loss_for:: Char -> Board -> Bool
+is_loss_for player board = (count player board) < size_of board
+
+count:: Char -> Board -> Int
+count c board = length (filter (== c) (dump_board board))
+
+other:: Char -> Char
+other player 
+    | player == 'B' = 'W'
+    | player == 'W' = 'B'
 
 -- Remove all null values from a list
 non_null [] = []
@@ -57,3 +96,7 @@ longest size = (size * 2) - 1
 row_size board_size current_row = ((2 * board_size) - abs(board_size - current_row) - 1)
 
 size_of board = length (head board)
+
+tiles_in board = (2*n - 1) + (n - 1) * (3*n - 2)
+    where 
+        n = size_of board
