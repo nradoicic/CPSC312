cebe3c694aa6fd738f8d65e86c4094beab65cf94
Nikola Radoicic <n.radoicic@gmail.com>
2014-11-18 22:05:37 -0800

    Added init which allows for user input for game items

--- project2/clue.pl
+++ project2/clue.pl
@@ -1,13 +1,69 @@
 :- use_module(library(lists)).
+:- style_check(-singleton).
 
-/* allow us to define cards dynamically */
-:- dynamic
-    suspect/1.
-:- dynamic
-    weapon/1.
-:-dynamic
-    room/1. %
-    
+init :-
+    retractall(player(X)),
+    retractall(suspect(X)),
+    retractall(weapon(X)),
+    retractall(room(X)),
+    retractall(card(X)),
+    write('Welcome to Clue!'),nl,
+    write('Please enter the names players playing today.'),nl,
+    write('Enter the word \'done\' to continue\'.'),nl,
+    input_players,nl,nl,
+    write('Please enter the suspects (characters) you\'d like available in the game.'),nl,
+    write('Enter the word \'done\' to continue\'.'),nl,
+    write('If you would like to just play a standard game of clue, simply type \'default\''),nl,
+    input_suspects,nl,nl,
+    write('Please enter the weapons you\'d like available in the game.'),nl,
+    write('Enter the word \'done\' to continue\'.'),nl,
+    input_weapons,nl,nl,
+    write('Please enter the rooms you\'d like available in the game.'),nl,
+    write('Enter the word \'done\' to continue\'.'),nl,
+    input_rooms,nl,nl,
+    write('Ready to play!'),nl,nl,
+    assert(card(X) :- room(X)),
+    assert(card(X) :- weapon(X)),
+    assert(card(X) :- suspect(X)),
+    game.
+
+input_players :-
+    write('    Player name: '),
+    read(X),
+    new_player(X).
+
+input_suspects :-
+    write('    Suspect name: '),
+    read(X),
+    new_suspect(X).
+
+input_weapons :-
+    write('    Weapon name: '),
+    read(X),
+    new_weapon(X).
+
+input_rooms :-
+    write('    Room name: '),
+    read(X),
+    new_room(X).
+
+new_player(done) :- !.
+new_player(X) :- assert(player(X)),input_players.
+new_suspect(done) :- !.
+new_suspect(default) :- default_suspects.
+new_suspect(X) :- assert(suspect(X)),input_suspects.
+new_weapon(done) :- !.
+new_weapon(default) :- default_weapons.
+new_weapon(X) :- assert(weapon(X)),input_weapons.
+new_room(done) :- !.
+new_room(default) :- default_rooms.
+new_room(X) :- assert(room(X)),input_rooms.
+
+
+game :-
+    !,
+    write('CLUE!!').
+
 /* in_hand(Player,Card) is true when we know a card is definitely in someone's hand
    this may happen when a card is shown to us and can be inferred at certain specific points */
 :- dynamic
@@ -25,38 +81,6 @@
    any of the suggested cards*/
 :- dynamic
     pass/4. %when someone passes on a suggestion
-
-/* define our domain
-
-In future this needs to be dynamic and inputtable
-additionally it would be nice if the number of cards was not fixed.*/
-suspect(peacock).
-suspect(mustard).
-suspect(plum).
-suspect(scarlet).
-suspect(white).
-suspect(green).
-
-weapon(wrench).
-weapon(pistol).
-weapon(rope).
-weapon(knife).
-weapon(lead).
-weapon(candle).
-
-room(hall).
-room(conservatory).
-room(kitchen).
-room(ballroom).
-room(billiard).
-room(library).
-room(studyr).
-room(lounge).
-room(dining).
-
-card(X) :- room(X).
-card(X) :- weapon(X).
-card(X) :- suspect(X).
 
 /* what could be in the envelope */
 envelope(X,Y,Z) :- possible_suspect(X),possible_weapon(Y),possible_room(Z).
@@ -82,3 +106,49 @@ certain(P) :- setof(X,in_hand(P,X),Hand), setof(X,hand_possibility(P,X),Poss), p
    ie we can eliminate things that are not in their hand as possibilities once 
    we know they have N cards if we know their hand has size N
    But I don't really want to do arithmetic*/
+
+
+% Defaults
+default_suspects :-
+    assert(suspect(peacock)),
+    assert(suspect(mustard)),
+    assert(suspect(plum)),
+    assert(suspect(scarlet)),
+    assert(suspect(white)),
+    assert(suspect(green)).
+
+default_weapons :-
+    assert(weapon(wrench)),
+    assert(weapon(pistol)),
+    assert(weapon(rope)),
+    assert(weapon(knife)),
+    assert(weapon(lead)),
+    assert(weapon(candle)).
+
+default_rooms :-
+    assert(room(hall)),
+    assert(room(conservatory)),
+    assert(room(kitchen)),
+    assert(room(ballroom)),
+    assert(room(billiard)),
+    assert(room(library)),
+    assert(room(studyr)),
+    assert(room(lounge)),
+    assert(room(dining)).
+
+def_init :-
+    retractall(player(X)),
+    retractall(suspect(X)),
+    retractall(weapon(X)),
+    retractall(room(X)),
+    retractall(card(X)),
+    assert(player(a)),
+    assert(player(b)),
+    assert(player(c)),
+    default_suspects,
+    default_weapons,
+    default_rooms,
+    assert(card(X) :- suspect(X)),
+    assert(card(X) :- weapon(X)),
+    assert(card(X) :- room(X)),
+    game.
